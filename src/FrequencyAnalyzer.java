c8a2a264abdadd57c57e3aa9dac17c252dbcc745
Jakub <jakubledzionlg@gmail.com>
2023-01-06 15:52:48 +0100

    Complete project

--- src/FrequencyAnalyzer.java
+++ src/FrequencyAnalyzer.java
@@ -1,18 +1,36 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class FrequencyAnalyzer implements Frequency{
-
-    public FrequencyAnalyzer() {
-    }
-
-    public Frequency getFrequency(){
-        return new FrequencyAnalyzer();
-    }
-
+public class FrequencyAnalyzer implements Frequency {
 
     @Override
-    public Map<Character, Double> analyze(String analyze) {
-        return null;
+    public Map<Character, Double> analyze(String str) {
+        Map<Character, Double> map = new HashMap<>();
+        if (str == null) {
+            throw new IllegalArgumentException("String must not be null!");
+        } else {
+
+            str = str.toLowerCase();
+            String strWithoutRepeat = "";
+            int strLeng = str.length();
+            for (int i = 0; i < strLeng; i++) {
+                if (str.isEmpty()) {
+                    break;
+                }
+                String letter = str.substring(0, 1);
+                strWithoutRepeat += letter;
+                str = str.replaceAll(letter, "");
+            }
+            double leng = strWithoutRepeat.length();
+            for (int i = 0; i < strWithoutRepeat.length(); i++) {
+                map.put(strWithoutRepeat.charAt(i), 1 / leng);
+            }
+        }
+
+        return map;
     }
+
+
 }
+
+
